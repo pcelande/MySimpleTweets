332cb13bb439cfd27b08e76ec5acb3fdd1d233e8
Celande Pierre <celandep@gmail.com>
2018-08-10 04:29:00 -0400

    first commit

---
+++ app/src/main/java/ht/gouv/budget/simplechat/ChatApplication.java
@@ -0,0 +1,28 @@
+package ht.gouv.budget.simplechat;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ChatApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("simplechat-client")
+                .clientBuilder(builder)
+                .server("https://codepath-chat-lab.herokuapp.com/parse/").build());
+
+    }
+}
